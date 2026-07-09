unorouter/new-api

feat(openapi): add OpenAPI spec generation for consumer-facing API routes
Adds optional OpenAPI 3.1 spec generation via Fuego (fuegogin adapter) for
the consumer-facing API routes (~60 endpoints). Admin, relay, and channel
management routes are left as plain Gin handlers, untouched.

Gated by ENABLE_OPENAPI=true env var (off by default). When enabled:
- GET /openapi.json returns the generated spec
- GET /swagger serves Scalar API Reference UI

No controller signatures are changed. Existing behavior is identical.
The Fuego engine wraps route registration to collect OpenAPI metadata
(tags, security schemes, request/response types) without affecting
the HTTP runtime.

dto/router.go
@@ -1,0 +1,198 @@
+package dto
+
+import (
+	"net/http"
+	"reflect"
+	"runtime"
+	"strings"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-fuego/fuego"
+	"github.com/go-fuego/fuego/extra/fuegogin"
+	"github.com/go-fuego/fuego/option"
+)
+
+// usedOperationIDs tracks operationIds to prevent collisions.
+var usedOperationIDs sync.Map
+
+// noopEngine is used when OpenAPI is disabled. fuegogin still needs a non-nil
+// engine to register the gin handler, but no spec metadata is collected.
+var noopEngine = fuego.NewEngine()
+
+// Router captures shared route registration parameters (engine, group, tag,
+// security) so individual route calls don't need to repeat them.
+type Router struct {
+	engine   *fuego.Engine
+	group    gin.IRouter
+	tag      string
+	basePath string
+	security []func(*fuego.BaseRoute)
+}
+
+// NewRouter creates a Router with shared defaults for a group of routes.
+func NewRouter(engine *fuego.Engine, group gin.IRouter, tag string, security ...func(*fuego.BaseRoute)) *Router {
+	base := ""
+	if rg, ok := group.(*gin.RouterGroup); ok {
+		base = rg.BasePath()
+	}
+	return &Router{engine: engine, group: group, tag: tag, basePath: base, security: security}
+}
+
+// WithTag creates a Router copy with a different OpenAPI tag.
+func (r *Router) WithTag(tag string) *Router {
+	return &Router{engine: r.engine, group: r.group, tag: tag, basePath: r.basePath, security: r.security}
+}
+
+// coreOpts prepends tag, security, summary, and operationId to user-provided options.
+// Falls back to a path-derived operationId when the handler name is already claimed.
+func (r *Router) coreOpts(method string, path string, handler any, extra []func(*fuego.BaseRoute)) []func(*fuego.BaseRoute) {
+	id := claimID(handlerID(handler))
+	if id == "" {
+		id = claimID(pathToOperationID(method, r.basePath+path))
+	}
+	opts := make([]func(*fuego.BaseRoute), 0, 3+len(r.security)+len(extra))
+	opts = append(opts, option.Tags(r.tag))
+	if id != "" {
+		opts = append(opts, option.Summary(handlerSummary(id)), option.OperationID(id))
+	}
+	opts = append(opts, r.security...)
+	opts = append(opts, extra...)
+	return opts
+}
+
+// claimID stores id in usedOperationIDs and returns it. If already taken or empty, returns "".
+func claimID(id string) string {
+	if id == "" {
+		return ""
+	}
+	if _, loaded := usedOperationIDs.LoadOrStore(id, true); loaded {
+		return ""
+	}
+	return id
+}
+
+// handlerID extracts the raw function name for use as an operationId.
+// e.g. controller.GetAllTokens -> "getAllTokens"
+// Returns empty string for anonymous functions (func1, func2, etc.)
+func handlerID(f any) string {
+	full := strings.TrimSuffix(runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name(), "-fm")
+	if idx := strings.LastIndex(full, "."); idx >= 0 {
+		full = full[idx+1:]
+	}
+	if strings.HasPrefix(full, "func") {
+		return ""
+	}
+	if len(full) > 0 {
+		full = strings.ToLower(full[:1]) + full[1:]
+	}
+	return full
+}
+
+// pathToOperationID converts an HTTP method and path to a camelCase operationId.
+// e.g. "GET", "/messages" -> "getMessages"
+// Wildcard params (*path) are skipped, named params (:id) are included.
+func pathToOperationID(method, path string) string {
+	method = strings.ToLower(method)
+	path = strings.TrimPrefix(path, "/")
+	parts := strings.Split(path, "/")
+	var b strings.Builder
+	b.WriteString(method)
+	for _, p := range parts {
+		if p == "" || p[0] == '*' {
+			continue
+		}
+		if p[0] == ':' {
+			p = p[1:]
+		}
+		for _, seg := range strings.Split(p, "-") {
+			if seg == "" {
+				continue
+			}
+			b.WriteString(strings.ToUpper(seg[:1]))
+			b.WriteString(seg[1:])
+		}
+	}
+	return b.String()
+}
+
+// handlerSummary converts a function name into a human-readable summary.
+// e.g. "getAllTokens" -> "Get All Tokens"
+func handlerSummary(id string) string {
+	if len(id) == 0 {
+		return ""
+	}
+	var b strings.Builder
+	b.WriteRune(rune(strings.ToUpper(id[:1])[0]))
+	for _, r := range id[1:] {
+		if 'A' <= r && r <= 'Z' {
+			b.WriteRune(' ')
+		}
+		b.WriteRune(r)
+	}
+	return b.String()
+}
+
+// --- Gin handler route registration ---
+
+// ginOpts builds options for raw gin handlers. Since gin handlers are registered
+// as Route[any,any,any], fuego cannot infer the response type. A default
+// ApiResponse schema is prepended; callers override it with GinResp[T]().
+func (r *Router) ginOpts(method, path string, handler gin.HandlerFunc, extra []func(*fuego.BaseRoute)) []func(*fuego.BaseRoute) {
+	core := r.coreOpts(method, path, handler, extra)
+	opts := make([]func(*fuego.BaseRoute), 0, 1+len(core))
+	opts = append(opts, fuego.OptionAddResponse(http.StatusOK, "OK", fuego.Response{Type: ApiResponse{}}))
+	opts = append(opts, core...)
+	return opts
+}
+
+// GinResp overrides the default ApiResponse schema for Gin* routes with a typed response.
+func GinResp[T any]() func(*fuego.BaseRoute) {
+	return fuego.OptionAddResponse(http.StatusOK, "OK", fuego.Response{Type: *new(T)})
+}
+
+// GinBody annotates a Gin handler route with a typed JSON request body in the OpenAPI spec.
+func GinBody[T any]() func(*fuego.BaseRoute) {
+	return fuego.OptionRequestBody(fuego.RequestBody{Type: *new(T), ContentTypes: []string{"application/json"}})
+}
+
+// TurnstileQuery adds an optional "turnstile" query parameter to the OpenAPI spec.
+func TurnstileQuery() func(*fuego.BaseRoute) {
+	return fuego.OptionQuery("turnstile", "Cloudflare Turnstile verification token")
+}
+
+// GinGet registers a raw gin handler GET route.
+func (r *Router) GinGet(path string, handler gin.HandlerFunc, opts ...func(*fuego.BaseRoute)) {
+	if r.engine == nil {
+		fuegogin.GetGin(noopEngine, r.group, path, handler)
+		return
+	}
+	fuegogin.GetGin(r.engine, r.group, path, handler, r.ginOpts("GET", path, handler, opts)...)
+}
+
+// GinPost registers a raw gin handler POST route.
+func (r *Router) GinPost(path string, handler gin.HandlerFunc, opts ...func(*fuego.BaseRoute)) {
+	if r.engine == nil {
+		fuegogin.PostGin(noopEngine, r.group, path, handler)
+		return
+	}
+	fuegogin.PostGin(r.engine, r.group, path, handler, r.ginOpts("POST", path, handler, opts)...)
+}
+
+// GinPut registers a raw gin handler PUT route.
+func (r *Router) GinPut(path string, handler gin.HandlerFunc, opts ...func(*fuego.BaseRoute)) {
+	if r.engine == nil {
+		fuegogin.PutGin(noopEngine, r.group, path, handler)
+		return
+	}
+	fuegogin.PutGin(r.engine, r.group, path, handler, r.ginOpts("PUT", path, handler, opts)...)
+}
+
+// GinDelete registers a raw gin handler DELETE route.
+func (r *Router) GinDelete(path string, handler gin.HandlerFunc, opts ...func(*fuego.BaseRoute)) {
+	if r.engine == nil {
+		fuegogin.DeleteGin(noopEngine, r.group, path, handler)
+		return
+	}
+	fuegogin.DeleteGin(r.engine, r.group, path, handler, r.ginOpts("DELETE", path, handler, opts)...)
+}

samlerneal/orbit-new-api

dto/router.go
@@ -1,0 +1,207 @@
+package dto
+
+import (
+	"net/http"
+	"reflect"
+	"runtime"
+	"strings"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-fuego/fuego"
+	"github.com/go-fuego/fuego/extra/fuegogin"
+	"github.com/go-fuego/fuego/option"
+)
+
+// usedOperationIDs tracks operationIds to prevent collisions.
+var usedOperationIDs sync.Map
+
+// noopEngine is used when OpenAPI is disabled. fuegogin still needs a non-nil
+// engine to register the gin handler, but no spec metadata is collected.
+var noopEngine = fuego.NewEngine()
+
+// Router captures shared route registration parameters (engine, group, tag,
+// security) so individual route calls don't need to repeat them.
+type Router struct {
+	engine   *fuego.Engine
+	group    gin.IRouter
+	tag      string
+	basePath string
+	security []func(*fuego.BaseRoute)
+}
+
+// NewRouter creates a Router with shared defaults for a group of routes.
+func NewRouter(engine *fuego.Engine, group gin.IRouter, tag string, security ...func(*fuego.BaseRoute)) *Router {
+	base := ""
+	if rg, ok := group.(*gin.RouterGroup); ok {
+		base = rg.BasePath()
+	}
+	return &Router{engine: engine, group: group, tag: tag, basePath: base, security: security}
+}
+
+// WithTag creates a Router copy with a different OpenAPI tag.
+func (r *Router) WithTag(tag string) *Router {
+	return &Router{engine: r.engine, group: r.group, tag: tag, basePath: r.basePath, security: r.security}
+}
+
+// coreOpts prepends tag, security, summary, and operationId to user-provided options.
+// Falls back to a path-derived operationId when the handler name is already claimed.
+func (r *Router) coreOpts(method string, path string, handler any, extra []func(*fuego.BaseRoute)) []func(*fuego.BaseRoute) {
+	id := claimID(handlerID(handler))
+	if id == "" {
+		id = claimID(pathToOperationID(method, r.basePath+path))
+	}
+	opts := make([]func(*fuego.BaseRoute), 0, 3+len(r.security)+len(extra))
+	opts = append(opts, option.Tags(r.tag))
+	if id != "" {
+		opts = append(opts, option.Summary(handlerSummary(id)), option.OperationID(id))
+	}
+	opts = append(opts, r.security...)
+	opts = append(opts, extra...)
+	return opts
+}
+
+// claimID stores id in usedOperationIDs and returns it. If already taken or empty, returns "".
+func claimID(id string) string {
+	if id == "" {
+		return ""
+	}
+	if _, loaded := usedOperationIDs.LoadOrStore(id, true); loaded {
+		return ""
+	}
+	return id
+}
+
+// handlerID extracts the raw function name for use as an operationId.
+// e.g. controller.GetAllTokens -> "getAllTokens"
+// Returns empty string for anonymous functions (func1, func2, etc.)
+func handlerID(f any) string {
+	full := strings.TrimSuffix(runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name(), "-fm")
+	if idx := strings.LastIndex(full, "."); idx >= 0 {
+		full = full[idx+1:]
+	}
+	if strings.HasPrefix(full, "func") {
+		return ""
+	}
+	if len(full) > 0 {
+		full = strings.ToLower(full[:1]) + full[1:]
+	}
+	return full
+}
+
+// pathToOperationID converts an HTTP method and path to a camelCase operationId.
+// e.g. "GET", "/messages" -> "getMessages"
+// Wildcard params (*path) are skipped, named params (:id) are included.
+func pathToOperationID(method, path string) string {
+	method = strings.ToLower(method)
+	path = strings.TrimPrefix(path, "/")
+	parts := strings.Split(path, "/")
+	var b strings.Builder
+	b.WriteString(method)
+	for _, p := range parts {
+		if p == "" || p[0] == '*' {
+			continue
+		}
+		if p[0] == ':' {
+			p = p[1:]
+		}
+		for _, seg := range strings.Split(p, "-") {
+			if seg == "" {
+				continue
+			}
+			b.WriteString(strings.ToUpper(seg[:1]))
+			b.WriteString(seg[1:])
+		}
+	}
+	return b.String()
+}
+
+// handlerSummary converts a function name into a human-readable summary.
+// e.g. "getAllTokens" -> "Get All Tokens"
+func handlerSummary(id string) string {
+	if len(id) == 0 {
+		return ""
+	}
+	runes := []rune(id)
+	var b strings.Builder
+	b.WriteRune([]rune(strings.ToUpper(string(runes[0])))[0])
+	for i := 1; i < len(runes); i++ {
+		prev := runes[i-1]
+		curr := runes[i]
+		next := rune(0)
+		if i+1 < len(runes) {
+			next = runes[i+1]
+		}
+		if curr >= 'A' && curr <= 'Z' {
+			if (prev >= 'a' && prev <= 'z') || ((prev >= 'A' && prev <= 'Z') && next >= 'a' && next <= 'z') {
+				b.WriteRune(' ')
+			}
+		}
+		b.WriteRune(curr)
+	}
+	return b.String()
+}
+
+// --- Gin handler route registration ---
+
+// ginOpts builds options for raw gin handlers. Since gin handlers are registered
+// as Route[any,any,any], fuego cannot infer the response type. A default
+// ApiResponse schema is prepended; callers override it with GinResp[T]().
+func (r *Router) ginOpts(method, path string, handler gin.HandlerFunc, extra []func(*fuego.BaseRoute)) []func(*fuego.BaseRoute) {
+	core := r.coreOpts(method, path, handler, extra)
+	opts := make([]func(*fuego.BaseRoute), 0, 1+len(core))
+	opts = append(opts, fuego.OptionAddResponse(http.StatusOK, "OK", fuego.Response{Type: ApiResponse{}}))
+	opts = append(opts, core...)
+	return opts
+}
+
+// GinResp overrides the default ApiResponse schema for Gin* routes with a typed response.
+func GinResp[T any]() func(*fuego.BaseRoute) {
+	return fuego.OptionAddResponse(http.StatusOK, "OK", fuego.Response{Type: *new(T)})
+}
+
+// GinBody annotates a Gin handler route with a typed JSON request body in the OpenAPI spec.
+func GinBody[T any]() func(*fuego.BaseRoute) {
+	return fuego.OptionRequestBody(fuego.RequestBody{Type: *new(T), ContentTypes: []string{"application/json"}})
+}
+
+// TurnstileQuery adds an optional "turnstile" query parameter to the OpenAPI spec.
+func TurnstileQuery() func(*fuego.BaseRoute) {
+	return fuego.OptionQuery("turnstile", "Cloudflare Turnstile verification token")
+}
+
+// GinGet registers a raw gin handler GET route.
+func (r *Router) GinGet(path string, handler gin.HandlerFunc, opts ...func(*fuego.BaseRoute)) {
+	if r.engine == nil {
+		fuegogin.GetGin(noopEngine, r.group, path, handler)
+		return
+	}
+	fuegogin.GetGin(r.engine, r.group, path, handler, r.ginOpts("GET", path, handler, opts)...)
+}
+
+// GinPost registers a raw gin handler POST route.
+func (r *Router) GinPost(path string, handler gin.HandlerFunc, opts ...func(*fuego.BaseRoute)) {
+	if r.engine == nil {
+		fuegogin.PostGin(noopEngine, r.group, path, handler)
+		return
+	}
+	fuegogin.PostGin(r.engine, r.group, path, handler, r.ginOpts("POST", path, handler, opts)...)
+}
+
+// GinPut registers a raw gin handler PUT route.
+func (r *Router) GinPut(path string, handler gin.HandlerFunc, opts ...func(*fuego.BaseRoute)) {
+	if r.engine == nil {
+		fuegogin.PutGin(noopEngine, r.group, path, handler)
+		return
+	}
+	fuegogin.PutGin(r.engine, r.group, path, handler, r.ginOpts("PUT", path, handler, opts)...)
+}
+
+// GinDelete registers a raw gin handler DELETE route.
+func (r *Router) GinDelete(path string, handler gin.HandlerFunc, opts ...func(*fuego.BaseRoute)) {
+	if r.engine == nil {
+		fuegogin.DeleteGin(noopEngine, r.group, path, handler)
+		return
+	}
+	fuegogin.DeleteGin(r.engine, r.group, path, handler, r.ginOpts("DELETE", path, handler, opts)...)
+}